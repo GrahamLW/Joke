e951a7cbab6b6a425bfe8343d668cf1e17149345
Graham Wood <grahamwood13@gmail.com>
2018-05-17 11:32:43 -0400

    Stupid Mistakes

--- app/src/main/java/edu/hanover/joke/MainActivity.java
+++ app/src/main/java/edu/hanover/joke/MainActivity.java
@@ -2,17 +2,12 @@ package edu.hanover.joke;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.content.Intent;
-import android.app.IntentService;
 
 public class MainActivity extends Activity {
 
-    public DelayedMessageService() {
-        super("DelayedMessageService");
-    }
-
     @Override
-    protected void onHandleIntent(Intent intent) {
-        //CODE HERE
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 }
